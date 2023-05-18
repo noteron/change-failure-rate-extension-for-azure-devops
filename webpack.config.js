418cfc540b8388eb409c9ff19860302aaa0df871
Christoffer Olsson <crippa9@users.noreply.github.com>
2023-05-18 22:13:11 +0200

    Replaced chokidar with webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -7,57 +7,68 @@ const entries = {};
 
 // Loop through subfolders in the "Contributions" folder and add an entry for each one
 const contributionsDir = path.join(__dirname, "src/Contributions");
-fs.readdirSync(contributionsDir).filter(dir => {
-    if (fs.statSync(path.join(contributionsDir, dir)).isDirectory()) {
-        entries[dir] = "./" + path.relative(process.cwd(), path.join(contributionsDir, dir, dir));
-    }
+fs.readdirSync(contributionsDir).filter((dir) => {
+  if (fs.statSync(path.join(contributionsDir, dir)).isDirectory()) {
+    entries[dir] =
+      "./" +
+      path.relative(process.cwd(), path.join(contributionsDir, dir, dir));
+  }
 });
 
 module.exports = {
-    entry: entries,
-    output: {
-        filename: "[name]/[name].js"
+  mode: "production",
+  target: "web",
+  optimization: {
+    minimize: true,
+  },
+  entry: entries,
+  output: {
+    filename: "[name]/[name].js",
+  },
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "azure-devops-extension-sdk": path.resolve(
+        "node_modules/azure-devops-extension-sdk"
+      ),
     },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {
-            "azure-devops-extension-sdk": path.resolve("node_modules/azure-devops-extension-sdk")
-        },
-    },
-    stats: {
-        warnings: false
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "azure-devops-ui/buildScripts/css-variables-loader", "sass-loader"]
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.woff$/,
-                use: [{
-                    loader: 'base64-inline-loader'
-                }]
-            },
-            {
-                test: /\.html$/,
-                loader: "file-loader"
-            }
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-           patterns: [ 
-               { from: "**/*.html", context: "src/Contributions" }
-           ]
-        })
-    ]
+    modules: [path.resolve("."), "node_modules"],
+  },
+  stats: {
+    warnings: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.woff$/,
+        use: [
+          {
+            loader: "base64-inline-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|html)$/,
+        loader: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "**/*.html", context: "src/Contributions" }],
+    }),
+  ],
 };
